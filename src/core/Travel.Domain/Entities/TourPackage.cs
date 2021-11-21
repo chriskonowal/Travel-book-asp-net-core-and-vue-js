e8c0f23736ceb3676ea5e8225b5459210f59dca6
Chris Konowal <chriskonowal@gmail.com>
2021-11-21 10:37:36 -0500

    Finishing Chapter 7

--- src/core/Travel.Domain/Entities/TourPackage.cs
+++ src/core/Travel.Domain/Entities/TourPackage.cs
@@ -14,6 +14,7 @@ namespace Travel.Domain.Entities
         public float Price { get; set; }
         public int Duration { get; set; }
         public bool InstantConfirmation { get; set; }
+        public string MapLocation { get; set; }
         public Currency Currency { get; set; }
         public TourList List { get; set; }
     }
