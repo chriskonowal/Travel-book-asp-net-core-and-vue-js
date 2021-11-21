cf3441877ae8ef019520b66ecc4b61580fe90ecc
Chris Konowal <chriskonowal@gmail.com>
2021-11-21 08:38:02 -0500

    Finishing controllers

--- src/presentation/Travel.WebApi/Controllers/TourListsController.cs
+++ src/presentation/Travel.WebApi/Controllers/TourListsController.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Travel.Data.Contexts;
 using Travel.Domain.Entities;
@@ -11,11 +8,11 @@ namespace Travel.WebApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class TourPackagesController : ControllerBase
+    public class TourListsController : ControllerBase
     {
         private readonly TravelDbContext _context;
 
-        public TourPackagesController(TravelDbContext context)
+        public TourListsController(TravelDbContext context)
         {
             _context = context;
         }
@@ -23,39 +20,42 @@ namespace Travel.WebApi.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(_context.TourPackages);
+            return Ok(_context.TourLists);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] TourPackage tourPackage)
+        public async Task<IActionResult> Create([FromBody] TourList tourList)
         {
-            await _context.TourPackages.AddAsync(tourPackage);
+            await _context.TourLists.AddAsync(tourList);
             await _context.SaveChangesAsync();
-            return Ok(tourPackage);
+
+            return Ok(tourList);
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            var tourPackage = await _context.TourPackages.SingleOrDefaultAsync(tp => tp.Id == id);
+            var tourList = await _context.TourLists.SingleOrDefaultAsync(tl => tl.Id == id);
 
-            if (tourPackage == null)
+            if (tourList == null)
             {
                 return NotFound();
             }
 
-            _context.TourPackages.Remove(tourPackage);
+            _context.TourLists.Remove(tourList);
             await _context.SaveChangesAsync();
 
-            return Ok(tourPackage);
+            return Ok(tourList);
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update([FromRoute]  int id, [FromBody] TourPackage tourPackage)
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] TourList tourList)
         {
-            _context.Update(tourPackage);
+            _context.Update(tourList);
+
             await _context.SaveChangesAsync();
-            return Ok(tourPackage);
+
+            return Ok(tourList);
         }
     }
 }
