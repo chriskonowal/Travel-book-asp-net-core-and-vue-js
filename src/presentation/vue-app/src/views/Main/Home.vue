8de3e487ae126a6fae744b2e52f52acf0e557869
Chris Konowal <chriskonowal@gmail.com>
2021-11-27 18:24:14 -0500

    Skeleton of site, chapter 12

---
+++ src/presentation/vue-app/src/views/Main/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home fill-height d-flex justify-center align-center">
+    <h1>Welcome to Travel Tours</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+
+  components: {
+  },
+};
+</script>
