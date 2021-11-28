45451235c48304a696263bec0a12af5f3b35eb02
Chris Konowal <chriskonowal@gmail.com>
2021-11-28 09:59:45 -0500

    Attaching Vue app to WebApi project, CORS

--- src/presentation/Travel.WebApi/Startup.cs
+++ src/presentation/Travel.WebApi/Startup.cs
@@ -15,6 +15,7 @@ using Travel.Shared;
 using Travel.WebApi.Extensions;
 using Travel.WebApi.Filters;
 using Travel.WebApi.Helpers;
+using VueCliMiddleware;
 
 namespace Travel.WebApi
 {
@@ -48,6 +49,13 @@ namespace Travel.WebApi
             services.AddSwaggerGenExtension();
 
             services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "../vue-app/dist";
+            });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,7 +65,14 @@ namespace Travel.WebApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwaggerExtension(provider);
+                app.UseSpaStaticFiles();
+                app.UseCors(b => {
+                    b.AllowAnyOrigin();
+                    b.AllowAnyHeader();
+                    b.AllowAnyMethod();
+                });
             }
+
 
             app.UseHttpsRedirection();
             app.UseRouting();
@@ -66,6 +81,16 @@ namespace Travel.WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "../vue-app";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseVueCli(npmScript: "serve");
+                }
             });
         }
     }
